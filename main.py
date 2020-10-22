2e5063485cad58b65cfcbbba8c95f0927f61e343
lokiplot <plotnikofften@gmail.com>
2020-10-22 22:53:51 +0300

    умеет заканчивать игру

--- main.py
+++ main.py
@@ -10,31 +10,7 @@ class Battlefield:
         self.actual_step = random.randint(0, 1)
         print(str(self.names[self.actual_step]) + ' goes first, his symbol is x')
 
-    def make_a_move(self, x, y):
-        """
-        (x, y) - the location of the cell, if the field is numbered as a two dimensional array
-        :param x:
-        :param y:
-        :return printing 'accepted' or 'impossible step':
-        """
-        if max(y, x) > 2 or min(x, y) < 0 or self.fields[x][y] != '0':
-            print('It\'s an impossible step')
-        else:
-            if self.number_of_steps % 2 == 0:
-                self.fields[x][y] = 'x'
-            else:
-                self.fields[x][y] = 'o'
-            print('Accepted, ' + str(self.names[self.actual_step]))
-            self.actual_step = 1 - self.actual_step
-
-    def print_battlefield(self):
-        for line in self.fields:
-            line_appearance = ''
-            for field in line:
-                line_appearance += field
-            print(line_appearance)
-
-    def print_whose_step(self):
+    def whose_step(self):
         return str(self.names[self.actual_step])
 
     def is_game_ended(self):
@@ -49,7 +25,43 @@ class Battlefield:
             is_ended = True
         return is_ended
 
+    def who_win(self):
+        if not self.is_game_ended():
+            return 0
+        return self.whose_step()
+
+    def make_a_move(self, x, y):
+        """
+        (x, y) - the location of the cell, if the field is numbered as a two dimensional array
+        :param x:
+        :param y:
+        :return printing 'accepted' or 'impossible step':
+        """
+        if self.is_game_ended():
+            print('Game ended')
+            return
+        if max(y, x) > 2 or min(x, y) < 0 or self.fields[x][y] != '0':
+            print('It\'s an impossible step')
+        else:
+            if self.number_of_steps % 2 == 0:
+                self.fields[x][y] = 'x'
+            else:
+                self.fields[x][y] = 'o'
+            print('Accepted, ' + str(self.names[self.actual_step]))
+            self.number_of_steps += 1
+            self.actual_step = 1 - self.actual_step
+
+    def print_battlefield(self):
+        for line in self.fields:
+            line_appearance = ''
+            for field in line:
+                line_appearance += field
+            print(line_appearance)
+
 
 b = Battlefield('1', '2')
+print (b.whose_step())
 b.make_a_move(0, 0)
+b.make_a_move(1, 1)
+b.make_a_move(0, 2)
 b.print_battlefield()
